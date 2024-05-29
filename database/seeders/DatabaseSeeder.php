4088fa4f880331a20e91be3bafd34bc0e6d92cf1
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2024-05-29 17:18:12 -0600

    Listado de carreras

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,44 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Admin',
-            'apellido' => 'Dios',
-            'email' => 'admin@admin.com',
-            'tipo' => 'admin',
-            'profile_update' => now(),
+        $this->call([
+            UserSeeder::class,
+            CarreraSeeder::class,
         ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'UsuarioA',
-            'apellido' => 'A',
-            'email' => 'a@gmail.com',
-            'tipo' => 'A',
-            'profile_update' => now(),
-        ]);
-
-        \App\Models\User::factory()->create([
-            'name' => 'UsuarioB',
-            'apellido' => 'B',
-            'email' => 'b@gmail.com',
-            'tipo' => 'B',
-            'profile_update' => now(),
-        ]);
-
-        \App\Models\User::factory()->create([
-            'name' => 'pruebaA',
-            'apellido' => 'A',
-            'email' => 'pruebaA@gmail.com',
-            'tipo' => 'A'
-        ]);
-
-        \App\Models\User::factory()->create([
-            'name' => 'pruebaB',
-            'apellido' => 'B',
-            'email' => 'pruebaB@gmail.com',
-            'tipo' => 'B'
-        ]);
     }
 }
