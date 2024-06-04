3a6caedb1f337cb57b9ece7eb3b7aae3e9e554f7
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2024-06-03 20:27:25 -0600

    Cambio de string por medium text en migracion de users a

--- database/migrations/2024_05_15_210359_create_users_a_table.php
+++ database/migrations/2024_05_15_210359_create_users_a_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->smallInteger('edad');
             $table->string('sexo');
-            $table->string('descripcion');
+            $table->mediumText('descripcion');
             $table->string('gustos_intereses');
             $table->string('carrera');
             $table->string('codigo');
