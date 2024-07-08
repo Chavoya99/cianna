5fc2d477609f639b5bf9f26ea5a19f444a9804a3
JulianValdivia18 <84482033+JulianValdivia18@users.noreply.github.com>
2024-07-07 23:19:52 -0600

    Update Front Home
    Versión final de forma estática

--- resources/views/profile/home.blade.php
+++ resources/views/profile/home.blade.php
@@ -1,60 +1,193 @@
 <!-- resources/views/home.blade.php -->
-@props(['defaultImage' => asset('img/home-add-svgrepo-com.png')])
+@props(['defaultHomeImage' => asset('img/home-add-svgrepo-com.png'), 'defaultProfileImage' => asset('img/avatar-default-svgrepo-com.png')])
 @section('title') {{'Inicio'}} @endsection
 <x-home-layout>
     <x-slot name="logo">
         <x-authentication-card-logo/>
     </x-slot>
-    <div class="w-full bg-red-500">
+    <div class="w-full">
         <div class="font-bold text-3xl mt-8 ml-16 mr-16">¿Buscas un hogar?</div>
         <div class="mt-2 ml-16">Lo recomendado para ti</div>
-        <div class="flex justify-between mt-2 ml-16 mr-16 bg-blue-300 overflow-hidden">
+        <div class="flex justify-between mt-2 ml-16 mr-16 overflow-hidden">
             <!-- IMAGEN CASA 1 -->
-            <div class="w-1/4 flex flex-col py-3 pr-3 bg-pink-300">
-                <div class="flex flex-col block bg-red-600">
-                    <div class="inline-block h-40 w-full overflow-hidden rounded-md bg-gray-100">
-                        <img class="object-cover border border-cianna-gray rounded-lg" src="{{ $defaultImage }}" alt="Imagen previa" />
+            <div class="w-1/4 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-44 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-home-1"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultHomeImage }}" alt="Imagen previa del hogar" /></a>
                     </div>
                 </div>
-                <h1 class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </h1>
-                <p class="text-sm line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</p>
+                <!-- COLONIA -->
+                <a href="detalle-home-1" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN -->
+                <a href="detalle-home-1" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
             </div>
             <!-- IMAGEN CASA 2 -->
-            <div class="w-1/4 flex flex-col py-3 pr-3 pl-3 bg-pink-300">
-                <div class="flex flex-col block bg-red-600">
-                    <div class="inline-block h-40 w-full overflow-hidden rounded-md bg-gray-100">
-                        <img class="object-cover border border-cianna-gray rounded-lg" src="{{ $defaultImage }}" alt="Imagen previa" />
+            <div class="w-1/4 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-44 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-home-2"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultHomeImage }}" alt="Imagen previa del hogar" /></a>
                     </div>
                 </div>
-                <h1 class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </h1>
-                <p class="text-sm line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</p>
+                <!-- COLONIA -->
+                <a href="detalle-home-2" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN -->
+                <a href="detalle-home-2" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
             </div>
             <!-- IMAGEN CASA 3 -->
-            <div class="w-1/4 flex flex-col py-3 pr-3 pl-3 bg-pink-300">
-                <div class="flex flex-col block bg-red-600">
-                    <div class="inline-block h-40 w-full overflow-hidden rounded-md bg-gray-100">
-                        <img class="object-cover border border-cianna-gray rounded-lg" src="{{ $defaultImage }}" alt="Imagen previa" />
+            <div class="w-1/4 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-44 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-home-3"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultHomeImage }}" alt="Imagen previa del hogar" /></a>
                     </div>
                 </div>
-                <h1 class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </h1>
-                <p class="text-sm line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</p>
+                <!-- COLONIA -->
+                <a href="detalle-home-3" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN -->
+                <a href="detalle-home-3" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
             </div>
             <!-- IMAGEN CASA 4 -->
-            <div class="w-1/4 flex flex-col py-3 pl-3 bg-pink-300">
-                <div class="flex flex-col block bg-red-600">
-                    <div class="inline-block h-40 w-full overflow-hidden rounded-md bg-gray-100">
-                        <img class="object-cover border border-cianna-gray rounded-lg" src="{{ $defaultImage }}" alt="Imagen previa" />
+            <div class="w-1/4 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-44 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-home-4"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultHomeImage }}" alt="Imagen previa del hogar" /></a>
                     </div>
                 </div>
-                <h1 class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </h1>
-                <p class="text-sm line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur libero non nunc ullamcorper, in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</p>
+                <!-- COLONIA -->
+                <a href="detalle-home-4" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN -->
+                <a href="detalle-home-4" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
             </div>
         </div>
-        <div class="text-right mr-16 mt-2"><a href="see-homes">Ver más...</a></div>
+        <div class="text-right mr-20 mt-2"><a class="text-cianna-green font-semibold hover:text-cianna-orange" href="see-homes">Ver más...</a></div>
     </div>
-    <div class="w-full bg-red-500">
+    <!-- ROOMIES -->
+    <div class="w-full">
         <div class="font-bold text-3xl mt-8 ml-16 mr-16">¿Buscas un compañero de cuarto?</div>
         <div class="mt-2 ml-16">Lo recomendado para ti</div>
-        <div class="text-right mr-16"><a href="see-rommies">Ver más...</a></div>
+        <div class="flex justify-between mt-2 ml-16 mr-16 overflow-hidden">
+            <!-- IMAGEN ROOMMIE 1 -->
+            <div class="w-1/6 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-36 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-roommie-1"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultProfileImage }}" alt="Imagen previa del hogar" /></a>
+                    </div>
+                </div>
+                <!-- NOMBRE ROOMIE -->
+                <a href="detalle-roommie-1" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN ROOMIE -->
+                <a href="detalle-roommie-1" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
+            </div>
+            <!-- IMAGEN ROOMMIE 2 -->
+            <div class="w-1/6 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-36 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-roommie-2"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultProfileImage }}" alt="Imagen previa del hogar" /></a>
+                    </div>
+                </div>
+                <!-- NOMBRE ROOMIE -->
+                <a href="detalle-roommie-2" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN ROOMIE -->
+                <a href="detalle-roommie-2" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
+            </div>
+            <!-- IMAGEN ROOMMIE 3 -->
+            <div class="w-1/6 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-36 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-roommie-3"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultProfileImage }}" alt="Imagen previa del hogar" /></a>
+                    </div>
+                </div>
+                <!-- NOMBRE ROOMIE -->
+                <a href="detalle-roommie-3" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN ROOMIE -->
+                <a href="detalle-roommie-3" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
+            </div>
+            <!-- IMAGEN ROOMMIE 4 -->
+            <div class="w-1/6 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-36 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-roommie-4"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultProfileImage }}" alt="Imagen previa del hogar" /></a>
+                    </div>
+                </div>
+                <!-- NOMBRE ROOMIE -->
+                <a href="detalle-roommie-4" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN ROOMIE -->
+                <a href="detalle-roommie-4" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
+            </div>
+            <!-- IMAGEN ROOMMIE 5 -->
+            <div class="w-1/6 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-36 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-roommie-5"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultProfileImage }}" alt="Imagen previa del hogar" /></a>
+                    </div>
+                </div>
+                <!-- NOMBRE ROOMIE -->
+                <a href="detalle-roommie-5" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN ROOMIE -->
+                <a href="detalle-roommie-5" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
+            </div>
+            <!-- IMAGEN ROOMMIE 6 -->
+            <div class="w-1/6 flex flex-col py-3 pr-3">
+                <div class="flex flex-col block">
+                    <div class="inline-block h-36 w-full overflow-hidden rounded-md bg-gray-100 relative">
+                        <a href="detalle-roommie-6"><img class="object-cover w-full h-full absolute top-0 left-0 border border-cianna-gray rounded-lg" 
+                        src="{{ $defaultProfileImage }}" alt="Imagen previa del hogar" /></a>
+                    </div>
+                </div>
+                <!-- NOMBRE ROOMIE -->
+                <a href="detalle-roommie-6" class="mt-2 text-lg font-semibold line-clamp-1">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin. </a>
+                <!-- DESCRIPCIÓN ROOMIE -->
+                <a href="detalle-roommie-6" class="text-sm line-clamp-3">Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. In consectetur libero non nunc ullamcorper,
+                in fermentum purus aliquet. Morbi ultricies dapibus odio eu sollicitudin.</a>
+            </div>
+        </div>
+        <div class="text-right mr-20 mt-2"><a class="text-cianna-green font-semibold hover:text-cianna-orange" href="see-rommies">Ver más...</a></div>
     </div>
 </x-home-layout>
