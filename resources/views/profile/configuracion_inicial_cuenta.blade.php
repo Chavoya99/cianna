2364f2ffc9135b7b0a36b60eda77500cce80e0ee
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2024-07-20 00:04:47 -0600

    Corrección de manejo de old values en configuracion inicial cuenta

--- resources/views/profile/configuracion_inicial_cuenta.blade.php
+++ resources/views/profile/configuracion_inicial_cuenta.blade.php
@@ -7,7 +7,6 @@
     <!-- Authentication -->
     <x-configuracion-cuenta-card>
         @if ($errors->any())
-
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                 <strong class="font-bold">¡Algo salió mal!</strong>
                 <ul>
