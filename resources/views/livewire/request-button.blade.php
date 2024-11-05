cca2ecb8eb809605c9ffc604a82a346d742b02d0
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2024-11-04 19:30:55 -0600

    Redirección a postulaciones en casas cuando ya se han postulado

--- resources/views/livewire/request-button.blade.php
+++ resources/views/livewire/request-button.blade.php
@@ -8,11 +8,13 @@
             Postularse
         </button>
     @else
-        <button class="mt-4 w-3/4 bg-cianna-blue hover:bg-sky-900 text-white font-bold 
-            py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            <i class="mr-2 fa-solid fa-envelope-circle-check"></i>
-            Postulado
-        </button>
+        <a href="{{route('ver_postulaciones')}}">
+            <button class="mt-4 w-3/4 bg-cianna-blue hover:bg-sky-900 text-white font-bold 
+                py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                <i class="mr-2 fa-solid fa-envelope-circle-check"></i>
+                Postulado
+            </button>
+        </a>
     @endif
 </div>
 
