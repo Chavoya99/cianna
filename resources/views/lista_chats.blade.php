c03b70bb1660e1bd6a35eef8bd409c9098e718e6
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2025-04-01 17:33:32 -0600

    Corrección de error al no haber ultimo mensaje

--- resources/views/lista_chats.blade.php
+++ resources/views/lista_chats.blade.php
@@ -1,6 +1,35 @@
 @props(['defaultProfileImage' => asset('img/selfie_mujer.jpg')])
 
 @section('title') {{ 'Chats | Todos' }} @endsection
+<script>
+    function diffForHumans(fecha1, fecha2 = new Date()) {
+        const diferenciaEnMs = fecha2 - fecha1;
+
+        const segundos = Math.floor(diferenciaEnMs / 1000);
+        const minutos = Math.floor(segundos / 60);
+        const horas = Math.floor(minutos / 60);
+        const dias = Math.floor(horas / 24);
+        const semanas = Math.floor(dias / 7);
+        const meses = Math.floor(dias / 30);
+        const anios = Math.floor(dias / 365);
+
+        if (anios > 0) {
+                return `Hace ${anios} año${años > 1 ? 's' : ''}`;
+            } else if (meses > 0) {
+                return `Hace ${meses} mes${meses > 1 ? 'es' : ''}`;
+            } else if (semanas > 0) {
+                return `Hace ${semanas} semana${semanas > 1 ? 's' : ''}`;
+            } else if (dias > 0) {
+                return `Hace ${dias} día${dias > 1 ? 's' : ''}`;
+            } else if (horas > 0) {
+                return `Hace ${horas} hora${horas > 1 ? 's' : ''}`;
+            } else if (minutos > 0) {
+                return `Hace ${minutos} minuto${minutos > 1 ? 's' : ''}`;
+            } else {
+                return `Hace ${segundos} segundo${segundos > 1 ? 's' : ''}`;
+            }
+    }
+</script>
 
 <x-home-layout>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js'></script>
@@ -61,6 +90,18 @@
                                 <p id="dif_fecha" class="text-sm group-hover:text-white">
                                 </p>
                             </div>
+                            <script>
+                                const fechaUtc = new Date();  // Fecha UTC actual
+                                const fecha1 = new Date("{{$chat['ultimoMensaje']->fecha_hora}}") 
+                                const zona_horaria_js = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+                                if(zona_horaria_js != "America/Mexico_City" ){
+                                    fechaUtc.setHours(fechaUtc.getHours() - 6); //Resta 6 horas
+                                }
+                                
+                                const diferencia_fecha = document.getElementById("dif_fecha");
+                                diferencia_fecha.innerText = diffForHumans(fecha1, fechaUtc);
+                            </script>
                         @else
                             <!-- Si no hay mensajes -->
                             <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 
@@ -94,46 +135,6 @@
     </div>
 </x-home-layout>
 
-<script>
-    function diffForHumans(fecha1, fecha2 = new Date()) {
-        const diferenciaEnMs = fecha2 - fecha1;
 
-        const segundos = Math.floor(diferenciaEnMs / 1000);
-        const minutos = Math.floor(segundos / 60);
-        const horas = Math.floor(minutos / 60);
-        const dias = Math.floor(horas / 24);
-        const semanas = Math.floor(dias / 7);
-        const meses = Math.floor(dias / 30);
-        const anios = Math.floor(dias / 365);
-
-        if (anios > 0) {
-                return `Hace ${anios} año${años > 1 ? 's' : ''}`;
-            } else if (meses > 0) {
-                return `Hace ${meses} mes${meses > 1 ? 'es' : ''}`;
-            } else if (semanas > 0) {
-                return `Hace ${semanas} semana${semanas > 1 ? 's' : ''}`;
-            } else if (dias > 0) {
-                return `Hace ${dias} día${dias > 1 ? 's' : ''}`;
-            } else if (horas > 0) {
-                return `Hace ${horas} hora${horas > 1 ? 's' : ''}`;
-            } else if (minutos > 0) {
-                return `Hace ${minutos} minuto${minutos > 1 ? 's' : ''}`;
-            } else {
-                return `Hace ${segundos} segundo${segundos > 1 ? 's' : ''}`;
-            }
-    }
-
-    const fechaUtc = new Date();  // Fecha UTC
-    const zona_horaria_js = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    
-    if(zona_horaria_js != "America/Mexico_City" ){
-        fechaUtc.setHours(fechaUtc.getHours() - 6); //Resta 6 horas
-    }
-    
-    fecha1 = new Date("{{$chat['ultimoMensaje']->fecha_hora}}");
-
-    const diferencia_fecha = document.getElementById("dif_fecha");
-    diferencia_fecha.innerText = diffForHumans(fecha1, fechaUtc);
-</script>
 
 
